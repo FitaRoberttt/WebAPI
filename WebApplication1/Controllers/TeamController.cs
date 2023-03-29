cac4cd669ba02b3495249b88f8b9d65973bdf661
FitaRobert <fita.robert@yahoo.com>
2023-03-29 23:41:46 +0300

    Commit final

---
+++ WebApplication1/Controllers/TeamController.cs
@@ -0,0 +1,101 @@
+﻿using Core.Dtos;
+using Core.Services;
+using DataLayer.Dtos;
+using DataLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/teams")]
+    public class TeamController : ControllerBase
+    {
+        public TeamService teamService { get; set; }
+
+        public TeamController(TeamService teamService)
+        {
+            this.teamService = teamService;
+        }
+
+
+        [HttpGet("get-all")]
+        public ActionResult<List<Team>> GetAll()
+        {
+            var results = teamService.GetAll();
+
+            return Ok(results);
+        }
+
+        [HttpGet("get/{teamId}")]
+
+        public ActionResult<Team> GetById(int teamId)
+        {
+            var result = teamService.GetById(teamId);
+
+            if (result == null)
+            {
+                return BadRequest("Team not found");
+            }
+
+            return Ok(result);
+        }
+
+        //add team
+
+        [HttpPost("add-team")]
+
+        public ActionResult<bool> AddTeam([FromBody] TeamAddDto payload)
+        {
+            var result = teamService.AddTeam(payload);
+
+            if (result == false)
+            {
+                return BadRequest("Team not found");
+            }
+
+            return Ok(result);
+        }
+
+        //add player
+
+        [HttpPost("add-player")]
+
+        public ActionResult<bool> AddPlayer([FromBody] PlayerAddDto payload)
+        {
+            var result = teamService.AddPlayer(payload);
+
+            if (result == false)
+            {
+                return BadRequest("Player not found");
+            }
+
+            return Ok(result);
+        }
+
+        //delete team
+
+        [HttpDelete("delete-team/{teamId}")]
+
+        public ActionResult<bool> DeleteTeam(int teamId)
+        {
+            var result = teamService.DeleteTeam(teamId);
+
+            if (result == false)
+            {
+                return BadRequest("Team not found");
+            }
+
+            return Ok(result);
+        }
+
+      
+        
+        
+        
+
+
+
+
+
+    }
+}
